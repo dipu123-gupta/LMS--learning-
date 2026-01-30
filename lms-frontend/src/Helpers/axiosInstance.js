6d3a99339505c1d14fcd39fe300193e2f1c6f351
dipu123-gupta <kumardipu845420@gmail.com>
2026-01-30 10:27:05 +0530

    backend helth

--- lms-frontend/src/Helpers/axiosInstance.js
+++ lms-frontend/src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://lms-learning-backend.onrender.com/api/v1";
+const BASE_URL = "https://lms-learning-backend1.onrender.com/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
